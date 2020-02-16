9a551eee3fcb30d051220cdee48a6c777453cd50
Christopher Kennedy <chris@admindev.tech>
2020-02-16 17:10:28 -0600

    Pixels on the screen baby!

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,11 +1,21 @@
 #include <iostream>
+#include "./Game.h"
+#include "./Constants.h"
 
 
 int main(int argc, char *args[])
 {
-    // TODO:
-    // Add game logic
-    
-    std::cout << "Game is running..." << std::endl;
+    Game *game = new Game();
+    game->Initialize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    while(game->IsRunning()) 
+    {
+        game->ProcessInput();
+        game->Update();
+        game->Render();
+    }
+
+    game->Destroy();
+
 }
 
